75218415878c02ba97bae53cd79e4fab26db4496
Pedro Trotta <pedroforte1911@gmail.com>
2025-02-21 12:29:28 -0400

    fixing redirection without www

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,17 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+
+  async redirects() {
+    return [
+      {
+        source: "https://trotta.dev/:path*",
+        has: [{ type: "host", value: "trotta.dev" }],
+        destination: "https://www.trotta.dev/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
